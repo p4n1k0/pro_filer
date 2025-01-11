a5eaaaf169d0bf6cfd18f746a1474daa1cdd33d2
p4n1k0 <gabre.tarick@gmail.com>
2025-01-11 20:11:24 -0300

    inicia testes para a fun show_preview

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,12 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+
+def test_show_preview(capsys):
+    context = {
+      "all_files": [],
+      "all_dirs": []
+    }
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == "Found 0 files and 0 directories\n"
+
+
